307570952666e2198d6d4b943f6ae14b08930539
Gastón d'Hiriart <gastondh91@gmail.com>
2022-08-23 17:09:06 -0300

    changes in env

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 const developmentMode = import.meta.env.MODE === 'development'
 
 const LOCAL_NODE_BACKEND_URL = 'http://127.0.0.1:3000/api'
-const PROD_NODE_BACKEND_URL = 'http://34.200.57.157/api'
+const PROD_NODE_BACKEND_URL = 'http://getsmart.ar/api'
 
 export const NODE_BACKEND_URL = developmentMode
   ? LOCAL_NODE_BACKEND_URL

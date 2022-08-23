0644e93b3f6e875254319373db7737717fc2ca2e
Gastón d'Hiriart <gastondh91@gmail.com>
2022-08-23 10:32:30 -0300

    changes in node url

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,8 @@
-const developmentMode = import.meta.env.MODE === "development";
+const developmentMode = import.meta.env.MODE === 'development'
 
-const LOCAL_NODE_BACKEND_URL = "http://127.0.0.1:3000/api";
-const PROD_NODE_BACKEND_URL = "http://54.166.228.14/api";
+const LOCAL_NODE_BACKEND_URL = 'http://127.0.0.1:3000/api'
+const PROD_NODE_BACKEND_URL = 'http://34.200.57.157/api'
 
 export const NODE_BACKEND_URL = developmentMode
   ? LOCAL_NODE_BACKEND_URL
-  : PROD_NODE_BACKEND_URL;
+  : PROD_NODE_BACKEND_URL
